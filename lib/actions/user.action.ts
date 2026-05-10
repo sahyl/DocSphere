512d8161e53191d7657cc63b1d70beca3eeacd87
Mohammed Sahil Khan <94916289+sahyl@users.noreply.github.com>
2026-05-10 17:41:04 +0530

    Update user.action.ts

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -4,26 +4,34 @@ import { clerkClient } from "@clerk/nextjs/server"
 import { parseStringify } from "../utils"
 import { liveblocks } from "../liveblocks"
 
-export const getClerkUsers = async ({userIds}:{userIds:string[]}) => {
-    try {
-        const {data} =await clerkClient.users.getUserList({
-            emailAddress:userIds,
-        })
-        const users = data.map((user)=>({
-            id:user.id,
-            name:`${user.firstName} ${user.lastName}`,
-            email:user.emailAddresses[0].emailAddress,
-            avatar:user.imageUrl,
-        }))
-        const sortedUsers = userIds.map((email)=> users.find((user)=> user.email===email))
-        return parseStringify(sortedUsers)
+export const getClerkUsers = async ({
+  userIds,
+}: {
+  userIds: string[]
+}) => {
+  try {
+    const { data } = await clerkClient.users.getUserList({
+      emailAddress: userIds,
+    })
 
-        
-    } catch (error) {
-        console.log(`Error  while fetching users : ${error}`)
-        
-    }
+    const users = data.map((user) => ({
+      id: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      email: user.emailAddresses[0].emailAddress,
+      avatar: user.imageUrl,
+    }))
 
+    const sortedUsers = userIds
+      .map((email) =>
+        users.find((user) => user.email === email)
+      )
+      .filter(Boolean)
+
+    return parseStringify(sortedUsers)
+  } catch (error) {
+    console.log(`Error while fetching users: ${error}`)
+    return []
+  }
 }
 export const getDocumentUsers = async({roomId,currentUser,text}:{roomId:string;currentUser:string; text:string}) => {
     try {
